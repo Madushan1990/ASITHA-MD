46d509568e289cbe76799a97f06cdb842cf714c8
Madushan1990 <viduramadushan618@gmail.com>
2024-10-08 21:34:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=irBiBKJa#vwvlEC8n3dUa2Uek9qs4-UPTQQsQzxSLnIPebitb6SI",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
